362a4bcb94b2aed485a65dc04e1139b10f3bfff8
MRCL <marcelo17922@gmail.com>
2022-02-20 12:38:18 -0600

    master

---
+++ js/Fisica/conversor.js
@@ -0,0 +1,202 @@
+//! MENSAJE GENERAL
+function conversor() {
+  swal(
+    `A que propiedad vas a poner para realizar la conversion con su unidad?`,
+    {
+      buttons: {
+        Potencia: "Potencia",
+        cancel: "Cancelar",
+        distancia: "Distancia",
+        masa: "Masa",
+        temperatura: "Temperatura",
+      },
+    }
+  ).then((value) => {
+    switch (value) {
+      case "masa":
+        convertkg();
+        break;
+      case "distancia":
+        convertmetros();
+        break;
+      case "Potencia":
+        converthpw();
+        break;
+      case "temperatura":
+        temperatura();
+        break;
+    }
+  });
+}
+//!  CONVERSIONES A METROS
+//_-----------------------------MENSAJE
+function convertmetros() {
+  swal("Que quieres convertir a metros?", {
+    buttons: {
+      cancel: "Cancelar",
+      cm: "cm",
+      km: "km",
+    },
+  }).then((value) => {
+    switch (value) {
+      case "km":
+        km_a_m();
+        break;
+
+      case "cm":
+        cm_a_m();
+        break;
+
+      default:
+    }
+  });
+}
+//_-----------------------------OPERADOR
+function km_a_m() {
+  swal(`Introduce tus kilometros`, {
+    content: "input",
+  }).then((value) => {
+    swal(`Tienes: ${parseFloat(value) * 1000}m`);
+  });
+}
+function cm_a_m() {
+  swal(`Introduce tus centimetros`, {
+    content: "input",
+  }).then((value) => {
+    swal(`Tienes: ${(parseFloat(value) * 1) / 100}m`);
+  });
+}
+
+//!-----------------------------CONVERSIONES A KILO GRAMOS
+//_-----------------------------MENSAJE
+function convertkg() {
+  swal("Que quieres convertir a kilogramo?", {
+    buttons: {
+      cancel: "Cancelar",
+      g: "g",
+      T: "T",
+    },
+  }).then((value) => {
+    switch (value) {
+      case "T":
+        T_a_kg();
+        break;
+
+      case "g":
+        g_a_kg();
+        break;
+
+      default:
+    }
+  });
+}
+//_-----------------------------OPERADOR
+function T_a_kg() {
+  swal(`Introduce tus Toneladas`, {
+    content: "input",
+  }).then((value) => {
+    swal(`Tienes: ${parseFloat(value) * 1000}kg`);
+  });
+}
+function g_a_kg() {
+  swal(`Introduce tus gramos`, {
+    content: "input",
+  }).then((value) => {
+    swal(`Tienes: ${parseFloat(value * 1) / 1000}kg`);
+  });
+}
+
+//!---------CONVERSIONES DE WATTS Y HP---------
+//-----Mensaje
+function converthpw() {
+  swal("Que quieres convertir?", {
+    buttons: {
+      cancel: "Cancelar",
+      watts: "Watts",
+      hp: "hp",
+    },
+  }).then((value) => {
+    switch (value) {
+      case "hp":
+        hp_a_Watts();
+        break;
+
+      case "watts":
+        Watts_a_hp();
+        break;
+
+      default:
+    }
+  });
+}
+//-----Operador
+function Watts_a_hp() {
+  swal("Introduce los Watts", {
+    content: "input",
+  }).then((value) => {
+    swal(`Tienes: ${parseFloat(value) / 745.7} hp`);
+  });
+}
+function hp_a_Watts() {
+  swal(`Introduce los caballos de fuerza`, {
+    content: "input",
+  }).then((value) => {
+    swal(`Tienes: ${parseFloat(value) * 745.7} Watts`);
+  });
+}
+//!-------------------------------CONVERSION DE TEMPERATURA
+//mensaje
+function temperatura() {
+  swal(`Qué unidades de temperatura tienes?`, {
+    buttons: {
+      celsius: "Celsius",
+      fahrenheit: "Fahrenheit",
+      kelvin: "Kelvin",
+    },
+  }).then((value) => {
+    switch (value) {
+      case "celsius":
+        celsius();
+        break;
+      case "fahrenheit":
+        fahrenheit();
+        break;
+      case "kelvin":
+        kelvin();
+        break;
+    }
+  });
+}
+//- operador
+function celsius() {
+  swal(`Introduce tus grados Celsius`, {
+    content: "input",
+  }).then((value) => {
+    swal(
+      `Tienes: 
+      ${(parseFloat(value) * 9) / 5 + 32} °F
+      ${parseFloat(value) + 273.15} K`
+    );
+  });
+}
+function fahrenheit() {
+  swal(`Introduce tus grados Fahrenheit`, {
+    content: "input",
+  }).then((value) => {
+    swal(
+      `Tienes:
+      ${parseFloat(value) - 32 * (5 / 9)} °C
+      ${(parseFloat(value) - 32) * (5 / 9) + 273.15} K`
+    );
+  });
+}
+function kelvin() {
+  swal(`Introduce tus Kelvin`, {
+    content: "input",
+  }).then((value) => {
+    swal(`
+    Tienes:
+    ${((parseFloat(value) - 273.15) * 9) / 5 + 32} °F
+    ${value - 273.15} °C`);
+  });
+}
---
+++ js/Fisica/operador.js
@@ -0,0 +1,219 @@
+//!---------------------------------------------------------------------------ACELERACION
+function aceleracion() {
+  swal(`Que problema necesitas?`, {
+    buttons: {
+      cancel: "Cancelar",
+      infi: "Inicial-Final",
+      basica: "Basica",
+    },
+  }).then((value) => {
+    switch (value) {
+      case "cancel":
+        break;
+      case "infi":
+        aif();
+        break;
+      case "basica":
+        abasica();
+        break;
+      default:
+        break;
+    }
+  });
+}
+function abasica() {
+  let velocidad = parseFloat(prompt("Introduzca su velocidad"));
+  let tiempo = parseFloat(prompt("Introduzca el tiempo empleado"));
+  let r = velocidad / tiempo;
+  swal(
+    `
+  ACELERACIÓN`,
+    `Formula:
+    a = v/t
+    ------
+    Recursos:
+    v = ${velocidad}m/s
+    t =  ${tiempo}s
+    ------
+    Resultado:
+    a = ${r}m/s²
+  `
+  );
+}
+function aif() {
+  let vi = parseFloat(prompt("Ingresa tu velocidad inicial"));
+  let vf = parseFloat(prompt("Ingresa velocidad final"));
+  let tiempo = parseFloat(prompt("Ingresa el tiempo"));
+  let r = (vf - vi) / tiempo;
+  swal(
+    `ACELERACIÓN Vi-Vf`,
+    `Formula:
+    a = Vf - Vi/t 
+    -----
+    Recursos:
+    Vi = ${vi}m/s
+    Vf = ${vf}m/s
+    t = ${tiempo}s
+    -----
+    Resultado:
+    a = ${r}m/s²`
+  );
+}
+//!-------------------------------------------------------------------------------------EC
+function ecbasica() {
+  let masa = parseFloat(prompt(`Introduce la masa`));
+  let velocidad2 = parseFloat(prompt(`Introduce la velocidad al cuadrado`));
+  let r = velocidad2 * velocidad2 * masa * 0.5;
+  swal(
+    `
+  ENERGIA CINÉTICA`,
+    `Formula:
+  Ec = v²·m/2 
+  -----
+  Recursos:
+  v² = ${velocidad2}m/s²
+  m = ${masa}kg
+  ----
+  Resultado:
+  Ec = ${r}J
+  `
+  );
+}
+//!-------------------------------------------------------------------------------------EMEC
+function emecbasica() {
+  let Ec = parseFloat(prompt("Ingresa la Energía Cinética"));
+  let Ep = parseFloat(prompt("Ingresa la Energía Potencial"));
+  let r = Ec + Ep;
+  swal(
+    `ENERGIA MECÁNICA`,
+    `Formula:
+    Emec. = Ec + Ep
+    -----
+    Recursos:
+    Ec = ${Ec}J
+    Ep = ${Ep}J
+    -----
+    Resultado:
+    Emec. = ${r}J`
+  );
+}
+//!-------------------------------------------------------------------------------------EP
+function epbasica() {
+  let masa = parseFloat(prompt("Introduce la masa"));
+  let altura = parseFloat(prompt("Introduce la altura"));
+  let r = masa * altura * 9.81;
+  swal(
+    `
+  ENERGIA POTENCIAL`,
+    `Formula:
+  Ep = m·g·h
+  -----
+  Recursos:
+  m = ${masa}kg
+  h = ${altura}m
+  g = 9.81m/s²
+  -----
+  Resultado:
+  Ep = ${r}J
+  `
+  );
+}
+
+//!-------------------------------------------------------------------------------------FUERZA
+function fbasica() {
+  let masa = parseFloat(prompt("Introduzca su masa"));
+  let aceleracion = parseFloat(prompt("Introduzca su aceleración"));
+  let r = masa * aceleracion;
+  swal(
+    `
+  Fuerza`,
+    `Formula:
+  F = m·a
+  -----
+  Recursos:
+  m = ${masa}kg
+  a = ${aceleracion}m/s²
+  -----
+  Resultado:
+  F = ${r}N
+  `
+  );
+}
+
+//!-------------------------------------------------------------------------------------POTENCIA
+function pbasica() {
+  let tmec = parseFloat(prompt("Introduzca el trabajo mecánico"));
+  let t = parseFloat(prompt("Introduzca el tiempo empleado"));
+  let r = tmec * t;
+  swal(
+    `Potencia Basica`,
+    `Formula:
+    P = Tmec · t
+    ------
+    Recursos:
+    Tmec = ${tmec}
+    t = ${t}
+    ------
+    Resultado:
+    P = ${r} Watts`
+  );
+}
+
+//!-------------------------------------------------------------------------------------TMEC
+function tmecbasica() {
+  let fuerza = parseFloat(prompt("Ingrese la fuerza"));
+  let distancia = parseFloat(prompt("Ingrese la distancia"));
+  let r = fuerza * distancia;
+  swal(
+    `TRABAJO MECÁNICO`,
+    `Formula:
+    Tmec. = F·d
+    -----
+    Recursos: 
+    F = ${fuerza}N
+    d = ${distancia}m
+    -----
+    Resultado:
+    Tmec. = ${r}J`
+  );
+}
+//!-------------------------------------------------------------------------------------T
+function tbasica() {
+  let f = parseFloat(prompt("Introduzca la fuerza"));
+  let d = parseFloat(prompt("Introduzca la distancia"));
+  let r = f * d;
+  swal(
+    `Trabajo`,
+    `Formula:
+    W = f · d
+    ------
+    Recursos:
+    F = ${f}
+    d = ${d}
+    ------
+    Resultado: 
+    W = ${r}J`
+  );
+}
+
+//!-------------------------------------------------------------------------------------VELOCIDAD
+function vbasica() {
+  let distancia = parseFloat(prompt("Introduce tu distancia"));
+  let tiempo = parseFloat(prompt("Introduce el tiempo"));
+  let r = distancia / tiempo;
+  swal(
+    `Velocidad`,
+    `Formula:
+    v = d/t
+    -----
+    Recursos:
+    d = ${distancia}m
+    t = ${tiempo}s
+    -----
+    Resultado: 
+    v = ${r}m/s`
+  );
+}
+
+//!-------------------------------------------------------------------------------------
+//!-------------------------------------------------------------------------------------
